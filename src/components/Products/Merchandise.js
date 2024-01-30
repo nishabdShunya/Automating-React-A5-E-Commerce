8e150a0c47a9ecd536ebf93059acc08afb0af6f5
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-01-31 01:30:03 +0530

    Add. A5-T2-Building Footer & Adding Merchandise - With Solution

--- src/components/Products/Merchandise.js
+++ src/components/Products/Merchandise.js
@@ -1,3 +1,8 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import MerchItem from "./MerchItem";
 import tshirt from "../../assets/pf_tshirt.jpg";
 import coffeeMug from "../../assets/pf_coffee_mug.png";
 import wallet from "../../assets/pf_wallet.jpg";
@@ -24,7 +29,27 @@ const DUMMY_MERCHANDISE = [
 ];
 
 const Merchandise = () => {
-  return <></>;
+  return (
+    <div className="m-5">
+      <h2 className="text-center">MERCHANDISE</h2>
+      <Container className="m-5">
+        <Row lg={3} className="gy-5 gx-5">
+          {DUMMY_MERCHANDISE.map((merch) => {
+            return (
+              <Col key={merch.id}>
+                <MerchItem
+                  id={merch.id}
+                  title={merch.title}
+                  price={merch.price}
+                  imageSrc={merch.imageSrc}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Merchandise;
