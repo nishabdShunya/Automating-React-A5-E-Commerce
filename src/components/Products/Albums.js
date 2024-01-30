9f1ea23f4eb736c963fe72a08122201eb4a0e17a
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-01-31 00:45:56 +0530

    Add. A5-T1-Adding Albums - With Solution

--- src/components/Products/Albums.js
+++ src/components/Products/Albums.js
@@ -1,3 +1,8 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Album from "./Album";
 import theDivisionBell from "../../assets/the_division_bell.jpg";
 import theEndlessRiver from "../../assets/the_endless_river.jpg";
 import obscuredByClouds from "../../assets/obscured_by_clouds.jpg";
@@ -45,7 +50,27 @@ const DUMMY_ALBUMS = [
 ];
 
 const Albums = () => {
-  return <></>;
+  return (
+    <div className="m-5">
+      <h2 className="text-center">MUSIC</h2>
+      <Container className="m-5">
+        <Row lg={3} className="gy-5 gx-5">
+          {DUMMY_ALBUMS.map((album) => {
+            return (
+              <Col key={album.id}>
+                <Album
+                  id={album.id}
+                  title={album.title}
+                  price={album.price}
+                  imageSrc={album.imageSrc}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Albums;
