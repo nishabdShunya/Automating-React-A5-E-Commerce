b15ffbf2af3d9be7b6be0b64fee9da1b6109b83e
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-02-25 20:05:15 +0530

    Add:A5-T15-V8&V9-useSearchParams Hook | Sorting Customer Reviews

--- src/components/ProductDetails/CustomerReviews.js
+++ src/components/ProductDetails/CustomerReviews.js
@@ -1,9 +1,22 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { Link, useSearchParams } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { DUMMY_CUSTOMER_REVIEWS } from "../../constants";
 
 const CustomerReviews = () => {
+  const [searchParams, setSearchParams] = useSearchParams("?sort=desc");
+  const sortType = searchParams.get("sort");
+
+  const changeSortHandler = () => {
+    const newSortType = sortType === "desc" ? "asc" : "desc";
+    setSearchParams({ sort: newSortType });
+  };
+
+  const sortedReviews = DUMMY_CUSTOMER_REVIEWS.sort((a, b) => {
+    const order = sortType === "asc" ? 1 : -1;
+    return order * (a.id - b.id);
+  });
+
   return (
     <Fragment>
       <div className="d-flex justify-content-between align-items-center">
@@ -12,8 +25,11 @@ const CustomerReviews = () => {
           &times;
         </Link>
       </div>
+      <Button variant="outline-dark" onClick={changeSortHandler}>
+        See {sortType === "desc" ? "Oldest" : "Latest"} First
+      </Button>
       <Container>
-        {DUMMY_CUSTOMER_REVIEWS.map((r) => (
+        {sortedReviews.map((r) => (
           <Row
             key={r.id}
             className="border border-dark-subtle my-1 py-3 rounded"
