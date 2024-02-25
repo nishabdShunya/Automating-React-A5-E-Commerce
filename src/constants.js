0ce5c79b9f7af91a5aec2c5f165dfc11f5a57b0a
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-02-25 13:18:02 +0530

    Add: A5-T14-V4-Dynamic Routing | useParams Hook

---
+++ src/constants.js
@@ -0,0 +1,69 @@
+import theDivisionBell from "./assets/the_division_bell.jpg";
+import theEndlessRiver from "./assets/the_endless_river.jpg";
+import obscuredByClouds from "./assets/obscured_by_clouds.jpg";
+import theDarkSideOfMoon from "./assets/the_dark_side_of_moon.jpg";
+import meddle from "./assets/meddle.jpg";
+import animals from "./assets/animals.jpg";
+import tshirt from "./assets/pf_tshirt.jpg";
+import coffeeMug from "./assets/pf_coffee_mug.png";
+import wallet from "./assets/pf_wallet.jpg";
+
+export const DUMMY_ALBUMS = [
+  {
+    id: "a1",
+    title: "The Division Bell",
+    price: 10,
+    imageSrc: theDivisionBell,
+  },
+  {
+    id: "a2",
+    title: "The Endless Mirror",
+    price: 15,
+    imageSrc: theEndlessRiver,
+  },
+  {
+    id: "a3",
+    title: "Obscured By Clouds",
+    price: 7,
+    imageSrc: obscuredByClouds,
+  },
+  {
+    id: "a4",
+    title: "The Dark Side of Moon",
+    price: 9,
+    imageSrc: theDarkSideOfMoon,
+  },
+  {
+    id: "a5",
+    title: "Meddle",
+    price: 12,
+    imageSrc: meddle,
+  },
+  {
+    id: "a6",
+    title: "Animals",
+    price: 19,
+    imageSrc: animals,
+  },
+];
+
+export const DUMMY_MERCHANDISE = [
+  {
+    id: "m1",
+    title: "T-Shirt",
+    price: 15,
+    imageSrc: tshirt,
+  },
+  {
+    id: "m2",
+    title: "Coffee Mug",
+    price: 7,
+    imageSrc: coffeeMug,
+  },
+  {
+    id: "m3",
+    title: "Wallet",
+    price: 11,
+    imageSrc: wallet,
+  },
+];
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const ProductDetails = () => {
+  const params = useParams();
+  return <h1>{params.productId}</h1>;
+};
+
+export default ProductDetails;
